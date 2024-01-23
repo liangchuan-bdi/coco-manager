930f653e45a851dbab3fd7af7c1d4ebe17bff267
liangchuan-bdi <126461193+liangchuan-bdi@users.noreply.github.com>
2024-01-23 12:13:04 +0800

    Create generate_car.sh

---
+++ generate_car.sh
@@ -0,0 +1,1 @@
+python3 filter.py --input_json /media/$USER/XAVIER2T/development/rk3588/yolov7/coco/train2017.txt --output_json /media/$USER/XAVIER2T/development/rk3588/yolov7/coco/train2017_filtered.txt --categories car
